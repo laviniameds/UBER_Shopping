35421915374bbdd6c764a3096ec231ada8eb1d5f
laviniameds <laviniameds@icloud.com>
2017-12-10 16:21:18 -0300

    ->metodo de inserir compra no BD criado;

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -9,6 +9,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 import javax.swing.JOptionPane;
 import net.proteanit.sql.DbUtils;
 import javax.swing.table.DefaultTableModel;
@@ -26,7 +29,8 @@ public class Controller {
         ResultSet result = null;
         Connection con = ConectionBD.conectBD();
         
-        String sql = "SELECT * from compra where login_cliente = ?";
+        String sql = "SELECT valor, local, data"
+                + " from compra where login_cliente = ?";
         
         try{
             pst = con.prepareStatement(sql);
@@ -105,8 +109,33 @@ public class Controller {
         
     }
     
-    public void finalizarCompra(Cliente cliente, Compra compra, Entregador entregador, Integer avaliacao){
+    public static void finalizarCompra(Cliente cliente, Compra compra, Entregador entregador, Integer avaliacao) throws ClassNotFoundException{
         entregador.setAvaliacao(avaliacao+1);
         cliente.adicionarHistoricoDeCompra(compra);
+        
+        PreparedStatement pst = null;
+        ResultSet result = null;
+        Connection con = ConectionBD.conectBD();
+        
+        String sql = "INSERT INTO compra(login_cliente, login_entregador, valor, local, data) values (?, ?, ?, ?, ?)";
+        
+        try{
+
+            Calendar calendar = Calendar.getInstance();
+            java.util.Date currentDate = calendar.getTime();
+            java.sql.Date date = new java.sql.Date(currentDate.getTime());
+            
+            pst = con.prepareStatement(sql);
+            pst.setString(1, cliente.getLogin());
+            pst.setString(2, entregador.getLogin());   
+            pst.setString(3, String.valueOf(compra.getValorTotalProdutos()));   
+            pst.setString(4, compra.getLocal());   
+            pst.setString(5, String.valueOf(date));   
+            result = pst.executeQuery();
+            
+        }
+        catch(SQLException e){
+            return;            
+        }        
     }
 }
