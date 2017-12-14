6d62fd44180c8e39a5f12f631df30981fdeea864
laviniameds <laviniameds@icloud.com>
2017-12-14 19:52:53 -0300

    ->metodos do controllerentregador atualizados;

--- src/controller/ControllerEntregador.java
+++ src/controller/ControllerEntregador.java
@@ -21,6 +21,31 @@ import net.proteanit.sql.DbUtils;
  */
 public class ControllerEntregador {
     
+        private static Cliente cliente = null;
+        private static Compra compra = null;
+        private static TableModel modeloTabela = null;
+        
+        /**
+        * @return the cliente
+        */
+        public static Cliente getCliente() {
+            return cliente;
+        }
+
+        /**
+         * @return the compra
+         */
+        public static Compra getCompra() {
+            return compra;
+        }
+
+        /**
+         * @return the modeloTabela
+         */
+        public static TableModel getModeloTabela() {
+            return modeloTabela;
+        }
+    
         public static TableModel listarEntregasPendentes(String login) throws ClassNotFoundException{
         PreparedStatement pst = null;
         ResultSet result = null;
@@ -74,11 +99,10 @@ public class ControllerEntregador {
         }
     }
         
-        public static Compra buscarCompraRandomica(Entregador entregador) throws ClassNotFoundException{
+        public static void generateRamdomCompra(Entregador entregador) throws ClassNotFoundException{
             PreparedStatement pst = null;
             ResultSet result = null;
             Connection con = ConectionBD.conectBD();
-            Compra compra = null;
 
             String sql = "SELECT * FROM compra where login_entregador = ? and pendente = 'true' ORDER BY RANDOM() LIMIT 1";
 
@@ -89,19 +113,64 @@ public class ControllerEntregador {
 
                 if(result.next()){
                     compra = new Compra();
-
-                    compra.setLocal(result.getString("local"));
-                    compra.setValorTotalProdutos(result.getInt("valor"));   
-                    compra.setData(result.getDate("data"));
-
-                    return compra;
+                    getCompra().setData(result.getDate("data"));
+                    getCompra().setLocal(result.getString("local"));
+                    getCompra().setValorTotalProdutos(Double.parseDouble(String.valueOf(result.getFloat("valor"))));
+                    
+                    setCliente(result.getString("login_cliente"));
+                    listarProdutos(result.getInt("id_compra"));
                 }
                 else
-                    return null;
+                    return;
             }
             catch(SQLException e){
-                return null;            
+                return;            
             }   
         }
-    
+        
+        public static void listarProdutos(Integer id_compra) throws ClassNotFoundException{
+            PreparedStatement pst = null;
+            ResultSet result = null;
+            Connection con = ConectionBD.conectBD();
+
+            String sql = "SELECT descricao, preco, quantidade FROM produto WHERE id_compra = ?";
+
+            try{
+                pst = con.prepareStatement(sql);
+                pst.setInt(1, id_compra);        
+                result = pst.executeQuery();
+
+                modeloTabela = DbUtils.resultSetToTableModel(result);
+            }
+            catch(SQLException e){
+                return;            
+            }
+        } 
+        
+        public static void setCliente(String login) throws ClassNotFoundException{
+            PreparedStatement pst = null;
+            ResultSet result = null;
+            Connection con = ConectionBD.conectBD();
+
+            String sql = "SELECT * FROM cliente where login = ?";
+
+            try{
+                pst = con.prepareStatement(sql); 
+                pst.setString(1, login);
+                result = pst.executeQuery();
+
+                if(result.next()){
+                    cliente = new Cliente();
+                    getCliente().setNome(result.getString("nome"));
+                    getCliente().setAvaliacao(Double.parseDouble(String.valueOf(result.getFloat("avaliacao"))));
+                    getCliente().setLogin(login);                   
+                }
+                else
+                    return;
+            }
+            catch(SQLException e){
+                return;            
+            }   
+        }
+                   
 }
